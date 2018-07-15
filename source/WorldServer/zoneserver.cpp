046d84431c740cffb5874a7af0b81326e89177b5
Jonmichael Chambers <jon@tyrbo.net>
2018-07-15 00:03:45 -0600

    Ensure entire update loop is guarded

--- source/WorldServer/zoneserver.cpp
+++ source/WorldServer/zoneserver.cpp
@@ -1696,30 +1696,29 @@ bool ZoneServer::UpdateProcess() {
 			SendSpawnChanges();
 		}
 
-	}
+		if (!reloading) {
+			bool spawnRange = spawn_range.Check();
+			bool checkRemove = spawn_check_remove.Check();
 
-	if (!reloading) {
-		bool spawnRange = spawn_range.Check();
-		bool checkRemove = spawn_check_remove.Check();
+			MSpawnList.readlock(__FUNCTION__, __LINE__);
+			for (const auto& kv : spawn_list) {
+				const auto spawn = kv.second;
 
-		MSpawnList.readlock(__FUNCTION__, __LINE__);
-		for (const auto& kv : spawn_list) {
-			const auto spawn = kv.second;
+				if (spawn) {
+					if (spawnRange) {
+						CheckSpawnRange(spawn);
+					}
 
-			if (spawn) {
-				if (spawnRange) {
-					CheckSpawnRange(spawn);
-				}
-
-				if (checkRemove) {
-					CheckRemoveSpawnFromClient(spawn);
+					if (checkRemove) {
+						CheckRemoveSpawnFromClient(spawn);
+					}
 				}
 			}
-		}
-		MSpawnList.releasereadlock(__FUNCTION__, __LINE__);
+			MSpawnList.releasereadlock(__FUNCTION__, __LINE__);
 
-		if (spawn_check_add.Check()) {
-			CheckSendSpawnToClient();
+			if (spawn_check_add.Check()) {
+				CheckSendSpawnToClient();
+			}
 		}
 	}
 
