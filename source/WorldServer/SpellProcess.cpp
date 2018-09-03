1e9e2305ed3d2891d0ba9648152c9b48740528ec
Jonmichael Chambers <jon@tyrbo.net>
2018-09-03 11:00:43 -0600

    Enable persist through death

--- source/WorldServer/SpellProcess.cpp
+++ source/WorldServer/SpellProcess.cpp
@@ -1588,11 +1588,11 @@ void SpellProcess::RemoveSpellTimersFromSpawn(Spawn* spawn, bool remove_all, boo
 			for (auto spell : active_spells) {
 				auto client = spell->caster->GetZone()->GetClientBySpawn(spell->caster);
 
-				/*if (spell->spell->GetSpellData()->persist_though_death) {
+				if (spell->spell->GetSpellData()->persist_though_death) {
 					if (client && client->IsConnected()) {
 						continue;
 					}
-				}*/
+				}
 
 				if (spell->caster == spawn) {
 					/*if (spell->caster != spell->caster->GetZone()->GetSpawnByID(spell->initial_target) && spell->spell->GetSpellData()->friendly_spell && spell->spell->GetSpellData()->target_type == SPELL_TARGET_OTHER && !spell->spell->GetSpellData()->group_spell) {
